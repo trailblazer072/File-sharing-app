93d58d3c815be5989301e019cb2b7000793f49c7
Adarsh Raghuwanshi <adarshraghuwanshi072@gmail.com>
2025-12-07 15:42:31 +0530

    login.js corrected

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import AuthLayout from '../layouts/AuthLayout';
